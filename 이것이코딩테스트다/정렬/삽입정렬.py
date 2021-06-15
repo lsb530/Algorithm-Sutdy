e137e2b3bd9d6f041b27f7d852ea71c4bf56f557
boki <lsb530@naver.com>
2021-06-15 20:09:13 +0900

    이것이 코딩테스트다 - 정렬

---
+++ 이것이코딩테스트다/정렬/삽입정렬.py
@@ -0,0 +1,13 @@
+# 삽입정렬, 시간 복잡도 O(N^2)
+# 특징: 현재 리스트의 데이터가 거의 정렬되어있는 상태라면 매우 빠르게 동작한다(최선의 경우 O(N))
+#      거의 정렬되어 있다면, 퀵정렬보다 더 빠르다.
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):  # 인덱스 i부터 1까지 감소하며 반복하는 문법
+        if array[j] < array[j - 1]:  # 한 칸씩 왼쪽으로 이동
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:  # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+            break
+
+print(array)
